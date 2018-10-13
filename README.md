726cee34c2d53484c6aeab77e03332dac624150d
weizhongtan <me@weizhongtan.com>
2018-10-13 22:29:40 +0100

    Improve things
    Add docs
    Add usage
    Improve cli output
    Strip out income payments

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # csv-transform
+
 Transforms Capital One credit card transaction format into HomeBank format
+
+## Usage
+
+```sh
+csv-transform --in ./transactions.csv [--out transactions-homebank.csv]
+```
+
+## Todo
+
+- Make it configuration based
--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 #!/usr/bin/env node
+
 const csvjson = require('csvjson');
 const fs = require('fs');
+const { promisify } = require('util');
+const chalk = require('chalk');
+const argv = require('minimist')(process.argv.slice(2));
 
-const csvFilePath = process.argv[2];
-const csvData = fs.readFileSync(csvFilePath, { encoding: 'utf8' });
-const jsonData = csvjson.toObject(csvData, {
-  delimiter: ',', // optional
-  quote: '"', // optional
-});
-const transformed = jsonData.map(d => ({
-  date: d.date.replace(/T.*/, ''),
-  payment: 1, // credit card
-  info: '',
-  payee: '',
-  memo: d.description,
-  amount: -d.originalCurrencyAmount, // expense
-  category: '',
-  tags: 'csv-transfer',
-}));
-const transformedCsv = csvjson.toCSV(transformed, {
-  delimiter: ';',
-  wrap: false,
-}).replace(/\[\]\./g, '');
-const transformedFilePath = csvFilePath.replace(/.csv$/, '-homebank.csv');
-fs.writeFileSync(transformedFilePath, transformedCsv);
-console.log('wrote file:', transformedFilePath);
-console.log('################## OUTPUT ##################\n', transformedCsv);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const { log } = console;
+
+const printUsage = () => {
+    log(chalk.red('usage: csv-transform --in ./path/to/input.csv [--out ./path/to/output.csv]'));
+};
+
+(async () => {
+    if (!argv.in) {
+        printUsage();
+        return;
+    }
+
+    const inPath = argv.in;
+    const outPath = argv.out || inPath.replace(/.csv$/, '-homebank.csv');
+
+    const csvData = await readFile(inPath, { encoding: 'utf8' });
+    const jsonData = csvjson.toObject(csvData, {
+        delimiter: ',', // optional
+        quote: '"', // optional
+    });
+
+    const isIncome = str => /PAYMENT RECEIVED/.test(str);
+    const transformed = jsonData
+        .filter(d => !isIncome(d.description))
+        .map(d => ({
+            date: d.date.replace(/T.*/, ''),
+            payment: 1, // credit card
+            info: '',
+            payee: '',
+            memo: d.description,
+            amount: -d.originalCurrencyAmount, // expense
+            category: '',
+            tags: 'csv-transfer',
+        }));
+    const transformedCsv = csvjson.toCSV(transformed, {
+        delimiter: ';',
+        wrap: false,
+    }).replace(/\[\]\./g, '');
+
+    await writeFile(outPath, transformedCsv);
+
+    log(chalk.green('Transformed output file:'), chalk.magenta(outPath));
+    log(chalk.cyan('################## OUTPUT ##################'));
+    log(chalk.yellow(transformedCsv));
+})();
