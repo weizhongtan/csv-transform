eede57aa883243be2240358c4046e3bbf60e04ec
weizhongtan <me@weizhongtan.com>
2018-10-14 13:46:43 +0100

    Add config

---
+++ cap1-homebank.conf.js
@@ -0,0 +1,30 @@
+const isIncome = str => /PAYMENT RECEIVED/.test(str);
+
+const transformer = json => json
+    .filter(d => !isIncome(d.description))
+    .map(d => ({
+        date: d.date.replace(/T.*/, ''),
+        payment: 1, // credit card
+        info: '',
+        payee: '',
+        memo: d.description,
+        amount: -d.originalCurrencyAmount, // expense
+        category: '',
+        tags: 'csv-transfer',
+    }));
+
+const options = {
+    in: {
+        delimiter: ',', // optional
+        quote: '"', // optional
+    },
+    out: {
+        delimiter: ';',
+        wrap: false,
+    },
+};
+
+module.exports = {
+    transformer,
+    options,
+};
