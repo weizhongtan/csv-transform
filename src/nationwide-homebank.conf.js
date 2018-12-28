5aec8d17cc6723d86aef49d500a8689af987cfab
weizhongtan <me@weizhongtan.com>
2018-12-28 01:10:39 +0000

    Add nationwide.conf tests

--- src/nationwide-homebank.conf.js
+++ src/nationwide-homebank.conf.js
@@ -1,14 +1,5 @@
 const convertDate = (d, y) => (new Date(`06:00 ${d} ${y}`)).toISOString().slice(0, 10);
 
-// test data
-// const json = [
-//     { date: '21 Jul', memo: 'Effective Date beans', out: '23', in: null },
-//     { date: '21 Jul', memo: 'EE TOP UP VESTA 08081686076', out: '10', in: null },
-//     { date: '21 Jul', memo: '(Recurring VISA Transaction)', out: null, in: null },
-//     { date: '21 Jul', memo: 'Contactless Payment', out: '4.1', in: null },
-//     { date: '21 Jul', memo: 'TSGN - ST PANCRAS INTE LONDON', out: null, in: null },
-// ];
-
 const transformer = (json) => {
     const filtered = json.reduce((arr, entry) => {
         const prev = arr[arr.length - 1];
